e3e3731ac55d47e7a9c614653d915105fed4fc0c
ReactJS223 <149454187+ReactJS223@users.noreply.github.com>
2023-11-05 01:41:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript code for photo carousel (you can use a library like Slick, Owl Carousel, or create your own)
+// Example:
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+    });
+});
